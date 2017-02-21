e139c5536b92eaf95c033a7d22f3fab9b71c265c
Voltmeter Amperage <gcrevell@mtu.edu>
2017-02-21 13:00:18 -0500

    Testing on remote server

--- DownloadFile.py
+++ DownloadFile.py
@@ -5,40 +5,42 @@ import subprocess	# Needed to run shell commands
 import urllib		# Needed to decode name if url encoded
 import time
 
-def downloadFile(url, dest='/', name=None):
+def downloadFile(url, dest='/', name=None, retries=3, maxTime = 60):
 	'''Download a file and stream it to the destination on ACD.'''
 	
 	if name is None:
 		# Parse name from url
 		name = url[url.rfind('/') + 1:]
-		name = name[:name.find('?')]
+		if '?' in name:
+			name = name[:name.find('?')]
 	
-		urllib.unquote(name).decode('utf8')
+		name = urllib.unquote(name).decode('utf8')
 		
 	if name[0] is '-':
+		# acd_cli needs a double dash in front of arguments starting with a dash
 		name = '-- ' + name
 	
-	x = 3	# Number of retries
 	t = 2	# Wait time between requests
 
 	# While we have retries...
-	while x > 0:
+	while retries > 0:
 		try:
 			# Run shell command. This is blocking.
-			subprocess.check_output('curl -s -S "{}" | ./acd/acd_cli -i none stream --overwrite --quiet "{}" "{}"'.format(url, name, dest), shell = True )
+			ret = subprocess.check_call('curl -s "{}" | ./acd/acd_cli_custom.py -i none stream --overwrite --quiet "{}" "{}"'.format(url, name, dest), shell = True )
+			break
 		except KeyboardInterrupt:
 			# If we get a keyboard interrupt, raise it to exit
 			raise
 		except:
 			# Otherwise, decrement tries
-			x -= 1
-			if x == 0:
+			retries -= 1
+			if retries == 0:
 				# If 0 tries, skip waiting
 				break
 
 			# Wait, then double time. This allows for quick backoff for too many requests
 			time.sleep(t)
-			t *= 2
+			t = max(2 * t, maxTime)
 
 def main():
 	'''The main program. Parses arguments in format [URL] [Dest] {Name}'''
@@ -47,7 +49,7 @@ def main():
 		# Wrong number of arguments.
 		print 'Proper use: ' + sys.argv[0] + ' [URL] [Dest] {Name}'
 
-	downloadFile(sys.argv[1], sys.argv[2], sys.argv[3] if 3 < len(sys.argv) else None)
+	downloadFile(sys.argv[1], dest = sys.argv[2], name = sys.argv[3] if 3 < len(sys.argv) else None)
 
 
 if __name__ == '__main__':
